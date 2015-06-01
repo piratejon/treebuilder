ee85da5e60d2562c5f0027a00cca74036ae5f5fe
Jonathan Wesley Stone <js@piratejon.com>
2015-06-01 01:18:35 -0500

    use helper variables inside dom create_element to make it easier and more clear what manipulations are taking place

--- tbuild.js
+++ tbuild.js
@@ -1,188 +1,197 @@
 /*jslint browser:true */
 var tb = (function () {
     "use strict";
-    var translators;
+    var xml_translator, dom_translator;
 
-    translators = {
-        /*
-         * dom/xml provides:
-         *   create_element(text, [root])
-         *   get_next_sibling(elt)
-         *   get_previous_sibling(elt)
-         *   get_value(elt)
-         *   get_first_child(elt)
-         *   iterate_children(elt, fn)
-         *   append_child(dst, appendee)
-         */
-
-        'dom': {
-            // elt is an LI which contains a SPAN, INPUT, INPUT, INPUT, INPUT, INPUT, UL
-            'create_element': function (text) {
-                var li, child_elts, i, parent_this;
-
-                if (text === undefined) {
-                    text = '';
-                }
-
-                parent_this = this; // need to access own create_element
-
-                function get_target_element(e) {
-                    // <http://stackoverflow.com/a/1553668>
-                    var tgt;
-                    e = e || window.event;
-                    tgt = e.target || e.srcElement;
-                    if (tgt.nodeType === 3) {
-                        tgt = tgt.parentNode;
-                    }
-                    return tgt;
-                }
-
-                child_elts = [
-                    ['span', {
-                        'textContent':  '-',
-                        'onclick':      function (e) {
-                            var tgt;
-                            tgt = get_target_element(e).parentNode.lastElementChild;
-                            if (tgt.style.display === 'none') {
-                                tgt.style.display = 'block';
-                                tgt.parentNode.firstElementChild.textContent = '-';
-                            } else {
-                                tgt.style.display = 'none';
-                                tgt.parentNode.firstElementChild.textContent = '+';
-                            }
-                        }
-                    }],
-                    ['input', { 'type': 'text', 'value': text, 'size': 6 }],
-                    ['input', {
-                        'type':     'button',
-                        'value':    '+',
-                        'onclick':  function (e) {
-                            var tgt;
-                            tgt = get_target_element(e);
-                            tgt.parentElement.lastElementChild.appendChild(parent_this.create_element());
-                        }
-                    }],
-                    ['input', {
-                        'type':     'button',
-                        'value':    'x',
-                        'onclick':  function (e) {
-                            var tgt;
-                            tgt = get_target_element(e).parentElement;
-                            if (tgt !== null && tgt.parentElement !== null) {
-                                tgt.parentElement.removeChild(tgt);
-                            }
-                        }
-                    }],
-                    ['input', {
-                        'type':     'button',
-                        'value':    '^',
-                        'onclick':  function (e) {
-                            // swap with previous sibling if any
-                            var tgt;
-                            tgt = get_target_element(e).parentNode;
-
-                            if (tgt.previousElementSibling !== null && tgt.parentNode !== null) {
-                                tgt.parentNode.insertBefore(tgt, tgt.previousElementSibling);
-                            }
-                        }
-                    }],
-                    ['input', {
-                        'type':     'button',
-                        'value':    'v',
-                        'onclick':  function (e) {
-                            // swap with next sibling if any
-                            var tgt;
-                            tgt = get_target_element(e).parentNode;
-
-                            if (tgt.nextElementSibling !== null && tgt.parentNode !== null) {
-                                tgt.parentNode.insertBefore(tgt.nextElementSibling, tgt);
-                            }
-                        }
-                    }],
-                    ['ul', {}]
-                ];
-
-                li = document.createElement('li');
-
-                function element_from_descriptor(element_descriptor) {
-                    var elt, prop, key;
-                    elt = document.createElement(element_descriptor[0]);
-                    prop = element_descriptor[1];
-                    for (key in prop) {
-                        if (prop.hasOwnProperty(key)) {
-                            elt[key] = prop[key];
-                        }
-                    }
-                    return elt;
-                }
-
-                for (i = 0; i < child_elts.length; i += 1) {
-                    li.appendChild(element_from_descriptor(child_elts[i]));
-                }
-
-                return li;
-            },
-            'get_next_sibling': function (elt) {
-                return elt.nextElementSibling;
-            },
-            'get_previous_sibling': function (elt) {
-                return elt.previousElementSibling;
-            },
-            'get_value': function (elt) {
-                return elt.firstElementChild.nextElementSibling.value;
-            },
-            'get_first_child': function (elt) {
-                if (elt.lastElementChild !== null && elt.lastElementChild.tagName === 'UL') {
-                    return elt.lastElementChild.firstElementChild;
-                }
-                return null;
-            },
-            'iterate_children': function (elt, fn) {
-                var kid;
-                for (kid = this.get_first_child(elt); kid !== null; kid = this.get_next_sibling(kid)) {
-                    fn(kid);
-                }
-            },
-            'append_child': function (elt, elt2) {
-                elt.lastElementChild.appendChild(elt2);
+    function DefaultTranslator() {
+        this.get_next_sibling = function (e) {
+            return e.nextElementSibling;
+        };
+        this.get_previous_sibling = function (e) {
+            return e.previousElementSibling;
+        };
+        this.get_value = function (e) {
+            return e.value;
+        };
+        this.get_first_child = function (e) {
+            return e.firstElementChild;
+        };
+        this.iterate_children = function (e, fn) {
+            var kid;
+            for (kid = this.get_first_child(e); kid !== null; kid = this.get_next_sibling(kid)) {
+                fn(kid);
             }
-        },
+        };
+        this.append_child = function (dst, appendee) {
+            dst.appendChild(appendee);
+        };
+    }
+    
+    xml_translator = new DefaultTranslator();
+    xml_translator.get_value = function (e) {
+        return e.getAttribute('value');
+    };
+    xml_translator.create_element = function (text) {
+        var node, attr;
 
-        'xml': {
-            'create_element': function (text) {
-                var node, attr;
+        if (this.root === undefined) {
+            this.root = document.implementation.createDocument(null, 'nodetree', null);
+        }
+        attr = this.root.createAttribute('value');
+        attr.nodeValue = text;
+        node = this.root.createElement('node');
+        node.setAttributeNode(attr);
+        return node;
+    };
+    
+    dom_translator = new DefaultTranslator();
+    dom_translator.create_element = function (text) {
+        var parent_this, element_from_descriptor, node, children, buttons;
 
-                if (this.root === undefined) {
-                    this.root = document.implementation.createDocument(null, 'nodetree', null);
-                }
-                attr = this.root.createAttribute('value');
-                attr.nodeValue = text;
-                node = this.root.createElement('node');
-                node.setAttributeNode(attr);
-                return node;
-            },
-            'get_next_sibling': function (node) {
-                return node.nextElementSibling;
-            },
-            'get_previous_sibling': function (node) {
-                return node.previousElementSibling;
-            },
-            'get_value': function (node) {
-                return node.getAttribute('value');
-            },
-            'get_first_child': function (node) {
-                return node.firstElementChild;
-            },
-            'iterate_children': function (node, fn) {
-                var kid;
-                for (kid = this.get_first_child(node); kid !== null; kid = this.get_next_sibling(kid)) {
-                    fn(kid);
-                }
-            },
-            'append_child': function (node, node2) {
-                node.appendChild(node2);
+        if (text === undefined) {
+            text = '';
+        }
+
+        parent_this = this; // need to access own create_element
+
+        function get_target_element(e) {
+            // <http://stackoverflow.com/a/1553668>
+            var tgt;
+            e = e || window.event;
+            tgt = e.target || e.srcElement;
+            if (tgt.nodeType === 3) {
+                tgt = tgt.parentNode;
+            }
+            return tgt;
+        }
+
+        node = document.createElement('li');
+
+        function fill_up_children(elt, child_list) {
+            var i;
+            for (i = 0; i < child_list.length; i += 1) {
+                elt.appendChild(element_from_descriptor(child_list[i]));
             }
         }
+
+        function set_attributes(elt, attrs) {
+            var attr;
+            for (attr in attrs) {
+                if (attrs.hasOwnProperty(attr)) {
+                    elt[attr] = attrs[attr];
+                }
+            }
+        }
+        
+        element_from_descriptor = function (element_descriptor) {
+            var elt;
+            
+            elt = document.createElement(element_descriptor.tag);
+
+            if (element_descriptor.attributes !== undefined) {
+                set_attributes(elt, element_descriptor.attributes);
+            }
+
+            if (element_descriptor.children !== undefined) {
+                fill_up_children(elt, element_descriptor.children);
+            }
+
+            return elt;
+        };
+
+        fill_up_children(node, [
+            {
+                'tag': 'span',
+                'attributes': {
+                    'textContent':  '-',
+                    'onclick':      function (e) {
+                        if (children.style.display === 'none') {
+                            children.style.display = 'block';
+                            node.firstElementChild.textContent = '-';
+                        } else {
+                            children.style.display = 'none';
+                            node.firstElementChild.textContent = '+';
+                        }
+                    }
+                }
+            },
+            {
+                'tag': 'input',
+                'attributes': {
+                    'type': 'text',
+                    'value': text,
+                    'size': 6
+                }
+            }
+        ]
+            );
+        
+        buttons = document.createElement('div');
+        fill_up_children(buttons, [
+            {
+                'tag': 'span',
+                'attributes': {
+                    'textContent':  '+',
+                    'onclick':      function (e) {
+                        children.appendChild(parent_this.create_element());
+                    }
+                }
+            },
+            {
+                'tag': 'span',
+                'attributes': {
+                    'textContent':  'x',
+                    'onclick':      function (e) {
+                        if (node.parentElement !== null) {
+                            node.parentElement.removeChild(node);
+                        }
+                    }
+                }
+            },
+            {
+                'tag': 'span',
+                'attributes': {
+                    'textContent':  '^',
+                    'onclick':      function (e) {
+                        // swap with previous sibling if any
+                        if (node.previousElementSibling !== null && node.parentNode !== null) {
+                            node.parentNode.insertBefore(node, node.previousElementSibling);
+                        }
+                    }
+                }
+            },
+            {
+                'tag': 'span',
+                'attributes': {
+                    'textContent':  'v',
+                    'onclick':      function (e) {
+                        if (node.nextElementSibling !== null && node.parentNode !== null) {
+                            node.parentNode.insertBefore(node.nextElementSibling, node);
+                        }
+                    }
+                }
+            }
+        ]
+                         );
+        
+        node.appendChild(buttons);
+        
+        children = document.createElement('ul');
+        node.appendChild(children);
+
+        return node;
+    };
+    dom_translator.get_value = function (e) {
+        return e.firstElementChild.nextElementSibling.value;
+    };
+    dom_translator.get_first_child = function (e) {
+        if (e.lastElementChild !== null && e.lastElementChild.tagName === 'UL') {
+            return e.lastElementChild.firstElementChild;
+        }
+        return null;
+    };
+    dom_translator.append_child = function (e1, e2) {
+        e1.lastElementChild.appendChild(e2);
     };
 
     function remove_element_children(e) {
@@ -209,7 +218,7 @@ var tb = (function () {
         root = document.implementation.createDocument(null, 'nodetree', null);
 
         root.documentElement.appendChild(
-            translate(src_elt, translators.dom, translators.xml)
+            translate(src_elt, dom_translator, xml_translator)
         );
 
         return serializer(root);
@@ -219,7 +228,7 @@ var tb = (function () {
         remove_element_children(dst_elt);
 
         dst_elt.appendChild(
-            translate(src_doc, translators.xml, translators.dom)
+            translate(src_doc, xml_translator, dom_translator)
         );
     }
 
@@ -229,7 +238,6 @@ var tb = (function () {
             function (root) {
                 return (new window.XMLSerializer()).serializeToString(root);
             }
-                // document.implementation.createDocument(null, 'nodetree', null)
         );
     }
 
